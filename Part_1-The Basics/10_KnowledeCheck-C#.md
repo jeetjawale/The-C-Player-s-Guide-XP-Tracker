44c469b1147cea4c229e661d720fd5bc71147797
jeetjawale <jeetjawale03@gmail.com>
2025-01-10 13:47:47 +0530

    update

--- Part_1-The Basics/10_KnowledeCheck-C#.md
+++
@@ -1,9 +0,0 @@
-# 10 Knowledge Check C# 25 XP
-
-## Check your knowledge with the following questions:
-
-## 1. C# is a special-purpose language optimized for making web applications.
-### True
-
-## 2. What is the name of the framework that C# runs on?
-### .NET
---
+++ Part_1-The Basics/26_ConsolasandTelim/26_ConsolasandTelim.cs
@@ -0,0 +1,11 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bread is ready.");
+        Console.WriteLine("Who is the bread for?");
+        string a = Console.ReadLine();
+        Console.WriteLine("Noted: " +a+ " got bread.");
+    }
+}
