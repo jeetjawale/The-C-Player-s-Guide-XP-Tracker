1943d727fa13c6da79d3cd1bcb59a28b48778357
jeetjawale <jeetjawale03@gmail.com>
2025-01-18 18:54:41 +0530

    buy_inventory

---
+++ Part_1-The Basics/82_BuyingInventory/82_BuyingInventory.cs
@@ -0,0 +1,58 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The following items are available:");
+        Console.WriteLine("1 - Rope");
+        Console.WriteLine("2 - Torches");
+        Console.WriteLine("3 - Climbing Equipment");
+        Console.WriteLine("4 - Clean Water");
+        Console.WriteLine("5 - Machete");
+        Console.WriteLine("6 - Canoe");
+        Console.WriteLine("7 - Food Supplies");
+
+        Console.Write("What number do you want to see the price of? ");
+        int itemNumber = Convert.ToInt32(Console.ReadLine());
+
+        int price;
+        string itemName;
+
+        switch (itemNumber)
+        {
+            case 1:
+                price = 10;
+                itemName = "Rope";
+                break;
+            case 2:
+                price = 15;
+                itemName = "Torches";
+                break;
+            case 3:
+                price = 25;
+                itemName = "Climbing Equipment";
+                break;
+            case 4:
+                price = 1;
+                itemName = "Clean Water";
+                break;
+            case 5:
+                price = 20;
+                itemName = "Machete";
+                break;
+            case 6:
+                price = 200;
+                itemName = "Canoe";
+                break;
+            case 7:
+                price = 1;
+                itemName = "Food Supplies";
+                break;
+            default:
+                price = 0;
+                itemName = "Invalid choice";
+                break;
+        }
+        Console.WriteLine($"{itemName} costs {price} gold");
+    }
+}
